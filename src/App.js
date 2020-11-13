8a5c878cd3254a8b2a60ffe5b011feacbdf5604b
Evyatar Haim <evyataros1@gmail.com>
2020-11-13 13:42:06 +0200

    adding Message component which styling with Material-UI's Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import { Button, Input, FormControl,InputLabel } from '@material-ui/core';
+import Message from './components/Message';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([{username:''}, {text:''}]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    setUsername(prompt('Please enter your name'));
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([
+      ...messages, {username: username, text: input}
+    ]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-     
+      <h2>Welcome {username}</h2>
+      <FormControl>
+        <InputLabel>Enter a messege...</InputLabel>
+          <Input 
+                      value={input} 
+                      onChange={event => setInput(event.target.value)}/>
+          <Button variant="contained" 
+                  color="primary"
+                  disabled={!input}
+                  type="submit" 
+                  onClick={sendMessage}>Send Messege
+          </Button>
+</FormControl>
+          
+          {messages.map(message => (
+              <Message username={message.username}
+                       key={message.text}
+                       text={message.text} 
+              />
+            ))
+          }
     </div>
   );
 }
